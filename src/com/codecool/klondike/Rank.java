e1b8e8142e98da12c8e4133c458cb3b78233c1a1
Miklós Kondász <miklos.kondasz@gmail.com>
2019-03-12 15:58:33 +0100

    "rebase development into feature/enum"

---
+++ src/com/codecool/klondike/Rank.java
@@ -0,0 +1,22 @@
+package com.codecool.klondike;
+
+public enum Rank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+    public String lowerCaseRank () {
+        return toString().toLowerCase();
+    }
+
+}
