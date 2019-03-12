59f8fce19e20420f92f2dbe137dccdadb2531962
BertalanBalazs <bertalan10@gmail.com>
2019-03-12 11:20:33 +0100

    Shuffled deck is ready

--- src/com/codecool/klondike/Card.java
+++ src/com/codecool/klondike/Card.java
@@ -96,6 +96,7 @@ public class Card extends ImageView {
                 result.add(new Card(suit, rank, true));
             }
         }
+        Collections.shuffle(result);
         return result;
     }
 
